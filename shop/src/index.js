baa0098effc95fa96b084671c40ab60560ed2bb9
baghbani01 <m.a.d.s.m0802@gmail.com>
2023-07-25 23:08:43 +0330

    Initial commit

---
+++ shop/src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+import React from "react";
+import App from './app'
+import ReactDOM from "react-dom";
+const el =document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+root.render(<App/>);
