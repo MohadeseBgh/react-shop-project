64bbae287067d461a05486116151e2ed811ecc10
baghbani01 <m.a.d.s.m0802@gmail.com>
2023-07-25 23:40:22 +0330

    .

--- shop/src/app.js
+++ shop/src/app.js
@@ -4,7 +4,7 @@ const App = () => {
     return(
         <>
             <div className={"flex items-center justify-center h-screen w-screen bg-blue-200"}>
-                Hello world!!!!
+                Hello!!!!
             </div>
         </>
     );
