c543ff0f056503028a164e9fff4e719e1f111baf
baghbani01 <m.a.d.s.m0802@gmail.com>
2023-07-26 13:36:20 +0330

    first version of navbar

--- shop/src/app.js
+++ shop/src/app.js
@@ -9,15 +9,15 @@ const App = () => {
 
         <>
             <ItemProvider>
-                <div className={"flex flex-col items-center justify-center h-screen w-screen bg-white"}>
-                        <div className={"flex flex-row basis-1/12 w-full"}>
+                <div className={"flex flex-col items-center justify-center h-screen w-screen bg-[#CACACA]"}>
+                        <div className={" basis-1/12 w-full"}>
                             <NavBar/>
                         </div>
                         <div className={"flex flex-row basis-11/12 w-full"}>
                             <div className={"basis-1/3 overflow-auto overflow-x-hidden bg-gradient-to-b from-[#FFC26F] to-[#F9E0BB]"}>
                                 <MenuList/>
                             </div>
-                            <div className={"basis-2/3 border-l-2 border-black rounded-lg bg-[#F9E0BB]"}>
+                            <div className={"basis-2/3 bg-[#F9E0BB]"}>
                                 <ItemInfo/>
                             </div>
                         </div>
--- shop/src/components/navBar.jsx
+++ shop/src/components/navBar.jsx
@@ -1,7 +1,25 @@
+import {IoCartOutline} from "react-icons/io5";
+
 const NavBar = () => {
     return(
-        <div>
-            edrftgyui
+        <div className={"flex flex-row items-center justify-between"}>
+
+            <div className={"font-bold text-2xl italic pl-3"}>
+                {/*design icon in canva*/}
+                Fillo Cafe
+            </div>
+            <div className={""}>
+                order now!!!
+            </div>
+            <div className={"flex flex-row pr-5 items-center content-center justify-center"}>
+                <button className={"m-2"}>
+                    <IoCartOutline className={"text-2xl"}/>
+                </button>
+                <button className={"p-1 pl-4 pr-4 border-2 border-black rounded-lg m-2"}>
+                    sign in
+                </button>
+            </div>
+
         </div>
     )
 }
