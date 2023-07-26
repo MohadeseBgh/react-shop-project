dd811160b43076959011c8948ec5dc1be336982f
baghbani01 <m.a.d.s.m0802@gmail.com>
2023-07-27 01:34:44 +0330

    itemInfo style

--- shop/src/pages/ItemInfo.jsx
+++ shop/src/pages/ItemInfo.jsx
@@ -28,15 +28,19 @@ const ItemInfo = () => {
                   price : {item.price}
               </div>
               <div className={"flex flex-row border border-[#4A4A4A] rounded-3xl pl-5 pr-5 gap-3 bg-[#FFDD67]"}>
-                  <button className={"p-1"} onClick={increase}>+</button>
+                  <button className={"p-1 transition hover:-translate-1 hover:scale-125 duration-300"} onClick={increase}>+</button>
                   <p className={"p-2 m-1"}>{counter}</p>
-                  <button className={"p-1"} onClick={decrease}>-</button>
+                  <button className={"p-1 transition hover:-translate-1 hover:scale-125 duration-300"} onClick={decrease}>-</button>
               </div>
           </div>
-          <div className={"basis-1/2"}>
-              <img src={item.img} className={"rounded-3xl p-1"} alt={''}/>
+          <div className={"basis-1/2 flex flex-col"}>
+              <img src={item.img} className={"rounded-3xl p-1 shadow-inner drop-shadow-md shadow-gray-700 scale-90 "} alt={''}/>
+              <div className={"flex flex-row p-3 pr-5 gap-2"}>
+                  <img src={item.img} className={"w-1/3 h-5/6 rounded-3xl p-1 shadow-gray-400 shadow-inner"} alt={''}/>
+                  <img src={item.img} className={"w-1/3 h-5/6 rounded-3xl p-1 shadow-gray-400 shadow-inner"} alt={''}/>
+                  <img src={item.img} className={"w-1/3 h-5/6 rounded-3xl p-1 shadow-gray-400 shadow-inner"} alt={''}/>
+              </div>
           </div>
-
       </div>
   )
 }
