37b1ccbcdac3f5e52a133b00b375ce88d5ddcf5f
baghbani01 <m.a.d.s.m0802@gmail.com>
2023-07-26 12:24:59 +0330

    add navbar component

--- shop/src/app.js
+++ shop/src/app.js
@@ -2,17 +2,25 @@ import React from "react";
 import MenuList from "./pages/MenuList";
 import ItemInfo from "./pages/ItemInfo";
 import ItemProvider from "./context/ItemContext";
+import NavBar from "./components/navBar";
 
 const App = () => {
     return (
 
         <>
             <ItemProvider>
-                <div className={"flex items-center justify-center h-screen w-screen bg-white"}>
-                    <div className={"flex flex-row box-content h-2/3 w-7/12 border-2 rounded-lg border-black"}>
-                        <div className={"basis-1/3 overflow-auto overflow-x-hidden bg-gradient-to-b from-[#FFC26F] to-[#F9E0BB]"}><MenuList/></div>
-                        <div className={"basis-2/3 border-l-2 border-black rounded-lg bg-[#F9E0BB]"}><ItemInfo/></div>
-                    </div>
+                <div className={"flex flex-col items-center justify-center h-screen w-screen bg-white"}>
+                        <div className={"flex flex-row basis-1/12 w-full"}>
+                            <NavBar/>
+                        </div>
+                        <div className={"flex flex-row basis-11/12 w-full"}>
+                            <div className={"basis-1/3 overflow-auto overflow-x-hidden bg-gradient-to-b from-[#FFC26F] to-[#F9E0BB]"}>
+                                <MenuList/>
+                            </div>
+                            <div className={"basis-2/3 border-l-2 border-black rounded-lg bg-[#F9E0BB]"}>
+                                <ItemInfo/>
+                            </div>
+                        </div>
                 </div>
             </ItemProvider>
         </>
---
+++ shop/src/components/navBar.jsx
@@ -0,0 +1,8 @@
+const NavBar = () => {
+    return(
+        <div>
+            edrftgyui
+        </div>
+    )
+}
+export default NavBar;
